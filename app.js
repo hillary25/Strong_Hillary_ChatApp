d8eda5149a26f0cff9e2a55197eaa8f2a86e2391
Hillary <h_strong69707@fanshaweonline.ca>
2020-01-29 08:43:36 -0500

    adding socket stuff

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+// Add socket here
+const io = require('socket.io')();
+
 const port = process.env.PORT || 3030;
 
 // tell express where our static files are (js, images, css etc)
@@ -13,3 +16,18 @@ app.get('/', (req, res) => {
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
+
+// Attach our chat server to our app
+io.attach(server);
+
+// Socket is two way communciation between client and server
+io.on('connection', function(socket) { // socket is your connection
+    // When user enters chat app
+    console.log('a user has connected');
+    socket.emit('connected', {sID: socket.id, message: "new connection"}); // sID is a variable called socket ID
+    
+    // When user refreshes, exits out chat app
+    socket.on('disconnect', function() {
+        console.log('a user has disconnected');
+    })
+})
